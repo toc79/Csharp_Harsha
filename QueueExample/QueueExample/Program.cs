69290fb19942fce726e5bf62d7226adcc2ddb3ba
Tomislav Krnjak <tomislav.krnjak79@gmail.com>
2024-02-18 20:22:41 +0100

    new files form C# course

---
+++ QueueExample/QueueExample/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QueueExample
+{
+	class Program
+	{
+		static void Main()
+		{
+			//create an object of Queue class
+			Queue<string> queue = new Queue<string>();
+
+			//Enqueue (Add)
+			queue.Enqueue("Task 3");
+			queue.Enqueue("Task 5");
+			queue.Enqueue("Task 1");
+			queue.Enqueue("Task 4");
+			queue.Enqueue("Task 2");
+
+			//foreach
+			foreach(string item in queue)
+			{
+				Console.WriteLine(item);
+			}
+
+			//Dequeue
+			string dq = queue.Dequeue();
+			Console.WriteLine("Dequeue: " + dq); //Task 3
+
+			string dq2 = queue.Dequeue();
+			Console.WriteLine("Dequeue: " + dq2);
+
+			//Peek
+			string pk = queue.Peek();
+			Console.WriteLine("Peek; " + pk);
+			
+			//foreach
+			foreach (string item in queue)
+			{
+				Console.WriteLine(item);
+			}
+
+			Console.ReadKey();
+		}
+	}
+}
---
+++ StackExample/StackExample/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StackExample
+{
+	class Student
+	{
+		public int Marks { get; set; }
+		public int Rank{ get; set; }
+	}
+	
+	class Program
+	{
+		static void Main()
+		{
+			//create object of Stack
+			Stack<int> marks = new Stack<int>();
+
+			//Add
+			marks.Push(45);
+			marks.Push(61);
+			marks.Push(80);
+
+			//foreach
+			foreach(int item in marks)
+			{
+				Console.WriteLine(item);
+			}
+
+			Console.WriteLine("");
+
+			//create object of Stack for Student
+			Stack<Student> marks2 = new Stack<Student>();
+
+			//Add
+			marks2.Push(new Student() { Marks = 45 });
+			marks2.Push(new Student() { Marks = 61 });
+			marks2.Push(new Student() { Marks = 80 });
+
+			//Pop
+			//Student stu = marks2.Pop();
+			//Console.WriteLine("Pop: " + stu.Marks);
+
+			//Peek
+			Student stu = marks2.Peek();
+			Console.WriteLine("Peek: " + stu.Marks);
+
+			int r = 1;
+			//foreach
+			foreach (Student item in marks2)
+			{
+				item.Rank = r;
+				Console.WriteLine(item.Marks + ", " +item.Rank);
+				r++;
+			}
+
+			Console.ReadKey();
+		}
+	}
+}
