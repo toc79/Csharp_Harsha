4bdcc386caa671ac77e31fa84a7299129f04f55e
Tomislav Krnjak <tomislav.krnjak79@gmail.com>
2024-06-13 21:01:53 +0200

    commiting modified and not versioned files

---
+++ NullReferenceExample/NullReferenceExample/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+
+namespace NullReferenceExample
+{
+	public class BankAccount
+	{
+		public int AccountNumber { get; set; }
+		public string AccountHolderName { get; set; }
+		public double CurrentBalance { get; set; }
+
+	}
+
+	public class FundsTransfer
+	{
+		public void Transfer(BankAccount sourceAccount, BankAccount destinationAccount, double amount)
+		{
+			try
+			{
+				sourceAccount.CurrentBalance -= amount;
+				destinationAccount.CurrentBalance += amount;
+		}
+			catch (NullReferenceException ex) {
+				Console.WriteLine("Catch inside Transfer MEthod " +ex.Message);
+				Console.WriteLine("The destination account is null");
+				throw; //throwing exception to outer calling portion (Main method) of the code to stop transfor from Bob's accounts
+			}
+}
+	}
+	
+	internal class Program
+	{
+		static void Main()
+		{
+			try {			
+				BankAccount bobBankAccount = new BankAccount() { AccountNumber = 101, AccountHolderName = "Bob", CurrentBalance = 10000 };
+				BankAccount aliceBankAccount = new BankAccount() { AccountNumber = 102, AccountHolderName = "Alice", CurrentBalance = 5000 };
+
+				FundsTransfer fundsTransfer = new FundsTransfer();
+				fundsTransfer.Transfer(bobBankAccount, aliceBankAccount, 1000);
+
+				Console.WriteLine("Funds transfered");
+				Console.WriteLine("Bob balance: " + bobBankAccount.CurrentBalance);
+				Console.WriteLine("Alice balance: " + aliceBankAccount.CurrentBalance);
+
+
+				BankAccount stevenBankAccount = null;
+				fundsTransfer.Transfer(bobBankAccount, stevenBankAccount, 1000);
+				Console.WriteLine("Funds transfered Steven example");
+				Console.WriteLine("Bob balance: " + bobBankAccount.CurrentBalance);
+				Console.WriteLine("Steven balance: " + stevenBankAccount.CurrentBalance);
+
+			} catch (NullReferenceException ex) {
+				Console.WriteLine(ex.Message);
+				Console.WriteLine("The destination account is null");
+			}
+
+			Console.ReadKey();
+		}
+	}
+}
---
+++ XmlSerializerExample/XmlSerializerExample/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace XmlSerializerExample
+{
+	[Serializable]
+	public class Continent
+	{
+		public string ContinentName { get; set;}
+		public List<Country> Countries { get; set;}
+	}
+	
+	[Serializable]
+	public class Country
+	{
+		public int CountryID { get; set; }
+		public string CountryName { get; set; }
+	}
+	class Program
+	{
+		static void Main()
+		{
+			//create object
+			List<Continent> continents = new List<Continent>();
+			continents.Add(new Continent()
+			{
+				ContinentName = "Africa",
+				Countries = new List<Country>() 
+					{ new Country() {CountryID = 1, CountryName = "Sudan"},
+					 new Country() {CountryID = 2, CountryName = "Libiya"},
+					 new Country() {CountryID = 3, CountryName = "South Africa"}
+					}			
+			});
+			continents.Add(new Continent()
+			{
+				ContinentName = "Asia",
+				Countries = new List<Country>()
+					{ new Country() {CountryID = 4, CountryName = "Russia"},
+					 new Country() {CountryID = 5, CountryName = "China"},
+					 new Country() {CountryID = 6, CountryName = "India"}
+					}
+			});
+
+			continents.Add(new Continent()
+			{
+				ContinentName = "Europe",
+				Countries = new List<Country>()
+					{ new Country() {CountryID = 7, CountryName = "Russia"},
+					 new Country() {CountryID = 8, CountryName = "Ukraine"},
+					 new Country() {CountryID = 9, CountryName = "France"}
+					}
+			});
+
+			continents.Add(new Continent()
+			{
+				ContinentName = "North America",
+				Countries = new List<Country>()
+					{ new Country() {CountryID = 10, CountryName = "Canada"},
+					 new Country() {CountryID = 11, CountryName = "United States"},
+					 new Country() {CountryID = 12, CountryName = "Mexico"}
+					}
+			});
+
+			continents.Add(new Continent()
+			{
+				ContinentName = "South America",
+				Countries = new List<Country>()
+					{ new Country() {CountryID = 13, CountryName = "Brazil"},
+					 new Country() {CountryID = 14, CountryName = "Argentina"},
+					 new Country() {CountryID = 15, CountryName = "Peru"}
+					}
+			});
+
+			Country country = new Country() {CountryID = 1, CountryName = "Untided States" };
+			
+			//create object of XmlSerializer
+			XmlSerializer xmlSerializer = new XmlSerializer(typeof(Country));
+			
+			XmlSerializer xmlSerializerContinents = new XmlSerializer(typeof(List<Continent>));
+
+			string filePath = @"c:\practice\country.xml";
+			string filePathSW = @"c:\practice\country_stream_writer.xml";
+			string filePathContinents = @"c:\practice\continents.xml";
+
+			//in class it is used FileStream
+			FileStream fileStream = new FileStream(filePath, FileMode.Create, FileAccess.Write);
+
+			StreamWriter streamWriter = new StreamWriter(filePathSW);
+
+			StreamWriter streamWriterContinents = new StreamWriter(filePathContinents);
+
+			//serialize
+			xmlSerializer.Serialize(fileStream, country);
+			fileStream.Close();
+
+			Console.WriteLine("country.xml created");
+
+			//serialize stream writer
+			xmlSerializer.Serialize(streamWriter, country);
+			streamWriter.Close();
+			Console.WriteLine("country_stream_writer.xml created");
+
+
+			//serializable continents
+			xmlSerializerContinents.Serialize(streamWriterContinents, continents);
+			streamWriterContinents.Close();
+			Console.WriteLine("continates.xml created");
+
+
+			//Deserialize
+
+			FileStream fileStreamDeserialize = new FileStream(filePathContinents, FileMode.Open, FileAccess.Read);
+			List<Continent> continents_from_file =  xmlSerializerContinents.Deserialize(fileStreamDeserialize) as List<Continent>;
+			Console.WriteLine("\ncontinents.xml created");
+
+			foreach(Continent cont in continents_from_file)
+			{
+				Console.WriteLine("\n" +cont.ContinentName);
+
+				foreach(Country country_from_file in cont.Countries)
+				{
+					Console.Write(country_from_file.CountryName +", ");
+				}
+			}
+
+
+			System.Console.ReadKey();
+		}
+	}
+}
