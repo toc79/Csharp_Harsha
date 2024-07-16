3151ed881909cce14ad29453517ddde54611a692
Tomislav Krnjak <tomislav.krnjak79@gmail.com>
2024-07-16 21:37:42 +0200

    committing 93 files
    Signed-off-by: Tomislav Krnjak <tomislav.krnjak79@gmail.com>

---
+++ Assigment17_MedicalBotWithFieldsAndMethods/Assigment17_MedicalBotWithFieldsAndMethods/Program.cs
@@ -0,0 +1,237 @@
+﻿//Assignment: Create a medical bot application
+//The application can prescribe medication based on the patient's symptoms and age.
+//Your task is to create a C# console application that simulates a medical bot named Bob. Bob should be able to prescribe medication based on the symptoms of a patient passed as an argument.
+//You should create two classes - MedicalBot and Patient - to implement this functionality.
+//Part 1: MedicalBot class
+//The MedicalBot class should have a constant string variable named BotName with the value "Bob" that represents the name of the medical bot.
+//It should have a static method called GetBotName() that returns value of "BotName" constant.
+//The MedicalBot class should also have a method named PrescribeMedication() that takes a Patient object as an argument. The PrescribeMedication() method should determine the medication to prescribe based on the symptoms of the patient.
+//void PrescribeMedication(Patient patient)
+//If the patient's symptoms are a headache, prescribe ibuprofen.
+//If the patient's symptoms are skin rashes, prescribe diphenhydramine.
+//If the patient's symptoms are dizziness, check if the patient has a medical history of diabetes. If they do, prescribe metformin. Otherwise, prescribe dimenhydrinate.
+//The PrescribeMedication() method should invoke another local function called GetDosage() to specify dosage of the specified medicine.
+//string GetDosage(string medicineName)
+//The GetDosage() method receives he medicine name prescribed by the PrescribeMedication() method and returns corresponding dosage as follows:
+//If the medicine name is ibuprofen:
+//If the patient's age is less than 18, then the dosage is 400 mg. Otherwise, the dosage is 800 mg.
+//If the medicine name is diphenhydramine:
+//If the patient's age is less than 18, then the dosage is 50 mg. Otherwise, the dosage is 300 mg.
+//If the medicine name is dimenhydrinate:
+//If the patient's age is less than 18, then the dosage is 50 mg. Otherwise, the dosage is 400 mg.
+//If the medicine name is metformin, then the dosage is 500 mg for all patients.
+//Part 2: Patient class
+//The Patient class should have the following fields:
+//name: A string representing the name of the patient.
+//age: An integer representing the age of the patient.
+//gender: A string representing the gender of the patient.
+//medicalHistory: A string representing the medical history of the patient.
+//symptomCode: A string representing the code for the symptoms of the patient.
+//prescription: A string representing the medication prescribed to the patient.
+//The Patient class should also have essential set / get methods for all the above fields. The Get methods returns the value of corresponding field. The Set methods should perform appropriate validation and assign the value into the field only if a valid value is supplied as argument. The Set methods should return a boolean value indicating whether the value is valid or not. If the value is valid, it should return true; otherwise false. In case of invalid value, an error message should be returned as "out" parameter. In case of valid value, an empty string should be returned as error message as "out" parameter.
+//GetName(): Returns the name of the patient.
+//SetName(string name, out string errorMessage): Sets the name of the patient and validates it. Returns a bool indicating whether the name is valid or not, and an error message if it's invalid. The name should not be null or empty. Patient name should contain at least two or more characters.
+//GetAge(): Returns the age of the patient.
+//SetAge(int age, out string errorMessage): Sets the age of the patient and validates it. Returns a bool indicating whether the age is valid or not, and an error message if it's invalid. Patient age can't be negative. Patient age can't be greater than 100.
+//GetGender(): Returns the gender of the patient.
+//SetGender(string gender, out string errorMessage): Sets the gender of the patient and validates it. Returns a bool indicating whether the gender is valid or not, and an error message if it's invalid. Patient gender should be either Male, Female or Other
+//GetMedicalHistory(): Returns the medical history of the patient.
+//SetMedicalHistory(string medicalHistory): Sets the medical history of the patient. No specific validations need to be checked for this value.
+//SetSymptomCode(string symptomCode, out string errorMessage): Sets the symptom code of the patient. Symptom Code should either be S1, S2, or S3.
+//GetSymptoms(): Returns the symptom description based on the current value of 'symptomCode' field.
+//S1 / s1: Headache
+//S2 / s2: Skin rashes
+//S3 / s3: Dizziness
+//Any other value: Unknown
+//GetPrescription(): Returns the medication prescribed to the patient.
+//SetPrescription(string prescription): Sets the medication prescribed to the patient. No specific validations need to be checked for this value.
+//Part 3: Main() method
+//This application will be used by patient users. The Main() method (entry point) should read all inputs (patient name, age, gender, medical history, symptoms etc.) from the patient and call "PrescribeMedication()" method of MedicalBot class and display the result prescription that was returned by the PrescribeMedication() method.
+//Sample Output 1:
+//Hi, I'm Bob. I'm here to help you in your medication.
+//Enter your (patient) details:
+//Enter Patient Name: William
+//Enter Patient Age: 30
+//Enter Patient Gender: Male
+//Enter Medical History. Eg: Diabetes.Press Enter for None: Diabetes
+//Welcome, William, 30.
+//Which of the following symptoms do you have:
+//S1.Headache
+//S2.Skin rashes
+//S3. Dizziness
+//Enter the symptom code from above list (S1, S2 or S3): S3
+//Your prescription based on your age, symptoms and medical history:
+//metformin 500 mg
+//Thank you for coming.
+//Sample Output 2:
+//Hi, I'm Bob. I'm here to help you in your medication.
+//Enter your (patient) details:
+//Enter Patient Name: Alice
+//Enter Patient Age: 15
+//Enter Patient Gender: Female
+//Enter Medical History. Eg: Diabetes.Press Enter for None:
+//Welcome, Alice, 15.
+//Which of the following symptoms do you have:
+//S1.Headache
+//S2.Skin rashes
+//S3. Dizziness
+//Enter the symptom code from above list (S1, S2 or S3): S2
+//Your prescription based on your age, symptoms and medical history:
+//diphenhydramine 50 mg
+//Thank you for coming.
+
+class MedicalBot
+{
+	const string BotName = "Bob";
+	static string GetBobName()
+	{
+		return BotName;
+	}
+
+	void PrescribedMedication(Patient patient)
+	{
+	
+
+	}
+}
+
+class Patient
+{
+	private string name;
+	private int age;
+	private string gender;
+	private string medicalHistory;
+	private string symptomCode;
+	private string prescription;
+
+	public string GetName()
+	{
+		return name;
+	}
+
+	public bool SetName(string name, out string errorMessage)
+	{
+		if (name != null && name.Trim() != "" && name.Trim().Length >= 2) 
+		{
+			this.name = name;
+			errorMessage = "";
+			return true;
+		} 
+		else
+		{
+			errorMessage = "The entered name "+ name.ToString() +" is invalid!";
+			return false;
+		}
+	}
+
+	public int GetAge ()
+	{
+		return age;
+	}
+
+	public bool SetAge(int age, out string errorMessage)
+	{
+		if (age >= 0 && age <= 100)
+		{
+			this.age = age;
+			errorMessage = "";
+			return true;
+		}
+		else
+		{
+			errorMessage = "The entered age "+ age.ToString() +" is invalid!";
+			return false;
+		}
+	}
+
+	public string GetGender()
+	{
+		return gender;
+	}
+
+	public bool SetGender(string gender, out string errorMessage)
+	{
+		if (gender.Trim().ToUpper() == "MALE" || gender.Trim().ToUpper() == "FEMALE" || gender.Trim().ToUpper() == "OTHER")
+		{
+			this.gender = gender;
+			errorMessage = "";
+			return true;
+		}
+		else
+		{
+			errorMessage = "The entered gender "+ gender.ToString() +" is invalid!";
+			return false;
+		}
+	}
+
+	public string GetMedicalHistory()
+	{
+		return medicalHistory;
+	}
+
+	public void SetMedicalHistory(string medicalHistory)
+	{
+		this.medicalHistory = medicalHistory;
+	}
+
+	public bool GetSymptomCode(string symptomCode, out string errorMessage)
+	{
+		if (symptomCode.Trim().ToUpper() != "S1" && gender.Trim().ToUpper() != "FEMALE" && gender.Trim().ToUpper() != "OTHER")
+		{
+			this.symptomCode = symptomCode;
+			errorMessage = "";
+			return true;
+		}
+		else
+		{
+			errorMessage = "The entered gender " + gender.ToString() + " is invalid!";
+			return false;
+		}
+	}
+
+	public string GetSymptoms()
+	{
+		if (symptomCode == "S1" || symptomCode =="s1")
+			return "Headache";
+		if (symptomCode == "S2" || symptomCode =="s2")
+			return "Skin rashes";
+		if (symptomCode == "S3" || symptomCode =="s3")
+			return "Dizziness";
+		return "Unknown";
+	}
+
+	public string GetPrescription()
+	{
+		return prescription;
+	}
+
+	void SetPrescription(string prescription) 
+	{
+		this.prescription = prescription;
+	}
+}
+
+class Program
+{
+	static void Main()
+	{
+		Patient patient = new Patient();
+		string errorMessage = "";
+
+		System.Console.WriteLine("Hi, I'm Bob. I'm here to help you in your medication.");
+		System.Console.WriteLine("Enter your (patient) details:");
+		System.Console.Write("Enter Patient Name: ");
+		
+		string patientName = System.Console.ReadLine();
+
+
+		patient.SetName(patientName, out errorMessage);
+
+		System.Console.WriteLine("\nWelcome, " +patient.GetName()); //add age
+		
+
+
+		System.Console.ReadKey();
+
+	}
+}
---
+++ Assigment5_ConvertSecondsIntoMinutes/Assigment5_ConvertSecondsIntoMinutes/Program.cs
@@ -0,0 +1,40 @@
+﻿//Eg:
+//seconds = 288970
+//The given number of seconds is equivalent to "3 days, 8 hours, 16 minutes, 10 seconds"
+//Output: 3 days, 8 hours, 16 minutes, 10 seconds
+//Note: Don't use any control statements such as 'if' or loops.
+//Don't use any predefined API (classes or structs).
+class ConvertSecondsIntoMinutes
+{
+	static void Main()
+	{
+		int input = 288970;
+		int secondsToMinuteFactor = 60;
+		int minutesToHourFactor = 60;
+		int hoursToDayFactor = 24;
+
+		//calculating days
+		int days = input / secondsToMinuteFactor / minutesToHourFactor / hoursToDayFactor;
+		//calculating seconds for days
+		int daysInSeconds = days * hoursToDayFactor * minutesToHourFactor * secondsToMinuteFactor;
+		//calculating remaining seconds
+		int remainingSeconds = input - daysInSeconds;
+
+		//calculating hours from remaining/rest of the seconds
+		int hours = remainingSeconds / secondsToMinuteFactor / minutesToHourFactor;
+
+		//calculating remaining seconds 
+		int hoursInSeconds = hours * minutesToHourFactor * secondsToMinuteFactor;
+		remainingSeconds = remainingSeconds - hoursInSeconds;
+
+		//calculating minutes from remaining seconds
+		int minutes = remainingSeconds / secondsToMinuteFactor;
+
+		//calculating remaining seconds
+		int minutesInSeconds = minutes * secondsToMinuteFactor;
+		remainingSeconds = remainingSeconds - minutesInSeconds;
+
+		System.Console.WriteLine(days + " days, " + hours + " hours, " + minutes + " minutes, " + remainingSeconds + " seconds");
+		System.Console.ReadKey();
+	}
+}
