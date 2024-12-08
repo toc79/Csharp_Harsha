aaec2e1f5bc88e3f52e7faa4a43c6ea7a53af4c6
Tomislav Krnjak <tomislav.krnjak79@gmail.com>
2024-12-08 11:48:49 +0100

    commit-anje datoteka

--- Assignment44_CustomGenericClassesAndGenericsMethods/Assignment44_CustomGenericClassesAndGenericsMethods/Program.cs
+++ Assignment44_CustomGenericClassesAndGenericsMethods/Assignment44_CustomGenericClassesAndGenericsMethods/Program.cs
@@ -1,11 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography.X509Certificates;
+﻿//Assignment: Custom Generic Classes and Generic Methods
+//In this assignment, you will create a custom implementation of a key-value database using arrays.You will create a Database<TKey, TValue> class that provides basic functionality for adding, removing, and retrieving key-value pairs.
+
+//Part 1: Create a Custom Generic Class
+//Create a Database<TKey, TValue> class that implements a simple key-value database using two arrays. This class should have the following methods:
+
+//void Add(TKey key, TValue value): adds a new key- value pair to the database.
+
+//bool Remove(TKey key): removes the key-value pair with the specified key from the database. Returns true if the pair was removed, false otherwise.
+
+//bool TryGetValue(TKey key, out TValue value): retrieves the value associated with the specified key. Returns true if the key was found, false otherwise.
+
+//int Count { get; }: returns the number of key-value pairs in the database.
+
+
+//The Database<TKey, TValue> class should use two arrays to store the keys and values. When a new key- value pair is added to the database, it should be appended to the end of the arrays. If the arrays are full, they should be resized to twice their current size.
+
+//When a key-value pair is removed, the class should search the keys array for the key to remove. If the key is found, the corresponding value should be removed from the values array, and the remaining elements should be shifted to the left to fill the gap. If the number of elements in the arrays falls below one-fourth of the array capacity, the arrays should be resized to half their current size.
+
+//The TryGetValue method should search the keys array for the specified key and return the corresponding value if it is found.
+
+//Part 2: Create a Generic Method
+//Create a generic method Swap<T>(ref T a, ref T b) that takes two parameters of type T by reference and swaps their values.
+
+//Part 3: Test Your Code
+//Create a console application and use it to test your Database<TKey, TValue> class and Swap<T> method.
+
+//In the Main method of the console application, create a Database<int, string> object and add some key-value pairs to it. Then, use the Remove method to remove one of the pairs, and use the TryGetValue method to retrieve the value associated with another key. Print the number of key-value pairs in the database using the Count property.
+
+//Next, create two variables of type int and swap their values using the Swap< int > method.Then, create two variables of a custom class of your choice and swap their values using the Swap< T > method.
+
+//Sample output:
+//A pair of key/value has been added to the database.
+//A pair of key/value has been added to the database.
+//A pair of key/value has been added to the database.
+//A pair of key/value has been added to the database.
+//Before remove:
+//Count: 4
+//Value for key 2: Two
+//After remove:
+//Count: 3
+//a = 2, b = 1
+//c1.Value = 2, c2.Value = 1
+
+using System;
 
 namespace Assignment44_CustomGenericClassesAndGenericsMethods
 {
-	
+
 	public class DatabaseClass<TKey, TValue>
 	{
 		public TKey[] tkey = new TKey[3];
---
+++ FIleIOExampleAsyncAwait1/FIleIOExampleAsyncAwait1/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+class FileWriter
+{
+	public async Task WriteFile(string fileName, string data)
+	{
+		//StreamWriter writer = new StreamWriter(fileName);
+		//Task writerTask = writer.WriteAsync(data);
+		//await writerTask;
+		//shorthand for above, simplified version
+
+		using (StreamWriter writer = new StreamWriter(fileName)) 
+		{ 
+			await writer.WriteAsync(data); 
+		}
+		
+		//writer.Close(); insted of Close, you can use using block statment 
+	}
+}
+
+class FileReader
+{
+	public async Task<string> ReadFile(string fileName)
+	{
+		//StreamReader reader = new StreamReader(fileName);
+		//	//Task<string> readerTask = reader.ReadToEndAsync();
+		//	//string content =  reader.Result; // automatski vraća Result sadržaj
+		//	//shorthand for above, simplified version
+		//string content = await reader.ReadToEndAsync();
+		
+		//reader.Close();
+		//return content;
+
+		//or simplified
+		using (StreamReader reader = new StreamReader(fileName))
+		{
+			string content = await reader.ReadToEndAsync();
+			return content;
+		}
+
+
+	}
+}
+
+class Program
+{
+	public async static Task Main()
+	{
+		//Initialize
+		string fileName = @"c:\practice\india.txt";
+		FileWriter fileWriter = new FileWriter();
+		FileReader fileReader = new FileReader();
+
+		//Write date to file asynchronously
+		//Task writerTaskFromMain = fileWriter.WriteFile(fileName, "India is the most populos country by 2023");
+		//writerTaskFromMain.Wait(); //Block until the write operation is completed
+		//await writerTaskFromMain; // doesnt block. It block curent task, but without blocking the current thread
+		
+		//shorthand for above, simplified version
+		await fileWriter.WriteFile(fileName, "India is the most populos country by 2023");
+
+
+		//Read data from the file asynchronously
+		//Task<string> readerTask = fileReader.ReadFile(fileName);
+		//readerTask.Wait(); //Block the current thread until the read operation is completed.
+		//await readerTask;
+		//shorthand for above, simplified version
+		string content = await fileReader.ReadFile(fileName);
+
+		Console.WriteLine("File read.");
+		Console.WriteLine($"\nFile content: {content}");
+
+		Console.ReadKey();
+	}
+}
